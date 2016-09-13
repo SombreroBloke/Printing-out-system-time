5ed314618ff4d730822115bff89ba76df2c95ed0
Oscar Williams <owilliams@utcmediacityuk.org.uk>
2016-09-13 12:46:08 +0100

    Add files via upload

---
+++ TimeInJava.java
@@ -0,0 +1,15 @@
+
+package time.in.java;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class TimeInJava {
+
+    public static void main(String[] args) 
+    {
+    Calendar cal = Calendar.getInstance();
+    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+    System.out.println( sdf.format(cal.getTime()) );
+    }
+    
+}
